6fe50442a34531565b3ce8477502be050d7537ef
Neha Navgale <nn78d@mail.umkc.edu>
2019-05-05 17:25:28 -0500

    committing map

--- Phase-2/tweetsAnalysis/src/app/app.component.ts
+++ Phase-2/tweetsAnalysis/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Data} from './locationData';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tweetsAnalysis';
+  geo_data = [];
+  constructor(private http: HttpClient) {this.mapData(); }
+  mapData() {
+    this.http.get('https://pbbackendanalysis.herokuapp.com/api/byCountry').subscribe((data: Data[]) => {
+      data.forEach(y => {
+        this.geo_data.push([y.location, y.count]);
+      });
+      console.log(this.geo_data);
+    }, error => {});
+  }
 }
